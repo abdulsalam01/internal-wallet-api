df15294ef2f1219d1f752c8792fec3ae42d56dc6
abdulsalam <abdulsalam@MacBook-Pro-M2.local>
2024-09-14 23:25:12 +0700

    REF: remove unused code

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,10 +5,4 @@ class UserController < ApplicationController
 
     render json: { data: encrypted }
   end
-
-  def show
-    price_lib = PriceRapidLib.new
-
-    render json: { data: price_lib.get_prices([ "NIFTY 50", "BAJFINANCEEQN" ]) }
-  end
 end
